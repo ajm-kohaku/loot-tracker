18c952156db1a56531637748a5a06fa9c1bf0a3f
Amber Murphy <45693571+ajm-kohaku@users.noreply.github.com>
2022-01-29 22:04:40 -0500

    set up vite project and build basic table

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,76 @@
+<script setup>
+</script>
+
+<template >
+  <div>
+    <h1 class="display-4">Loot Tracker</h1>
+    <table class="table">
+      <thead>
+        <tr>
+          <th scope="col">item description</th>
+          <th scope="col">quantity</th>
+          <th scope="col">worth</th>
+          <th scope="col">total</th>
+          <th scope="col">location</th>
+          <th scope="col">date acquired</th>
+          <th scope="col">category</th>
+          <th scope="col">character</th>
+          <th scope="col">vendor price</th>
+          <th scope="col">sell?</th>
+          <th scope="col">sold?</th>
+          <th scope="col">date sold</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>diamond</td>
+          <td>2</td>
+          <td>500</td>
+          <td>1000</td>
+          <td>aradoon temple</td>
+          <td>1/15/2022</td>
+          <td>Trade</td>
+          <td></td>
+          <td>1000</td>
+          <td><input type="checkbox"></td>
+          <td><input type="checkbox"></td>
+          <td>1/22/2022</td>
+        </tr>
+        <tr>
+          <td>diamond</td>
+          <td>2</td>
+          <td>500</td>
+          <td>1000</td>
+          <td>aradoon temple</td>
+          <td>1/15/2022</td>
+          <td>Trade</td>
+          <td></td>
+          <td>1000</td>
+          <td><input type="checkbox"></td>
+          <td><input type="checkbox"></td>
+          <td>1/22/2022</td>
+        </tr>
+        <tr>
+          <td>diamond</td>
+          <td>2</td>
+          <td>500</td>
+          <td>1000</td>
+          <td>aradoon temple</td>
+          <td>1/15/2022</td>
+          <td>Trade</td>
+          <td></td>
+          <td>1000</td>
+          <td><input type="checkbox"></td>
+          <td><input type="checkbox"></td>
+          <td>1/22/2022</td>
+        </tr>
+        
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<style lang="scss">
+@import "../styles.scss";
+</style>
+
